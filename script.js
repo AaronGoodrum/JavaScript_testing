6f85fe700d97cb9fafc30d1967294ec86152685e
Aaron G <ag505181@gmail.com>
2019-09-25 08:46:17 -0700

    enumerable

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use strict'
 
 
-// Writable Attribute
+
 var cat = {
   name: {first: 'Fluffy', last: 'LeBeouf'},
   color: 'White'
 }
 console.log (cat.name)
-Object.defineProperty(cat, 'name', {writable: false})
 
+// Writable Attribute
+Object.defineProperty(cat, 'name', {writable: false})
 cat.name.first = 'Scratchy'
 console.log (cat.name)
+
+Object.defineProperty(cat, 'name', {enumerable: false})
