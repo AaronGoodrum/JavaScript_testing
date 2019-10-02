a668648762631c9f05636a64d69f2d8334b7bc31
Aaron G <ag505181@gmail.com>
2019-10-02 08:34:13 -0700

    creating objects

---
+++ task_Creation/task.js
@@ -0,0 +1,11 @@
+// var task = {};
+var task = Object.create(Object.prototype)
+
+task.title = 'My Task'
+task.description = 'My Description'
+task.toString = function(){
+  return this.title + ' ' + this.description
+}
+
+console.log(task.toString())
+// console.log(task.title)
