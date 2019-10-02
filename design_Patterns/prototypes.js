53ec76088585b4251df26c02014971be46808d28
Aaron G <ag505181@gmail.com>
2019-10-02 08:59:29 -0700

    building prototypes

---
+++ design_Patterns/prototypes.js
@@ -0,0 +1,40 @@
+//constructor pattern
+//create new objects with their oen object scope
+
+function ObjectName(param1, param2){
+  //binds 'this' to the new object scope
+  this.param1 = param1
+  this.param2 = param2
+  this.toString = function() {
+    return this.param1 + ' ' + this.param2
+  }
+}
+
+// 
+var Task = function(name){
+  this.name = name
+  this.completed = false
+
+  // this.complete = function (){
+  //   console.log('completing task: ' + this.name)
+  //   this.completed = true
+  // }
+  // this.save = function(){
+  //   console.log('saving Task: ' + this.name)
+  // }
+}
+
+Task.prototype.complete = function (){
+  console.log('completing task: ' + this.name)
+  this.completed = true
+}
+
+TextTrackList.prototype.save = function() {
+    console.log('saving Task: ' + this.name)
+  }
+
+var task1 = new Task('create a demo for Constructor')
+var task2 = new Task('create a demo for prototypes')
+
+task1.complete()
+task2.save()
